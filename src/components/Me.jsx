14978b887daefb396708085b0b079ea96072470f
Замалдинов Равиль <vanteateam@gmail.com>
2024-07-05 20:30:37 +0500

    Update Me.jsx

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -3,13 +3,16 @@ import Photo from "./Photo.jsx";
 import Social from "./Social.jsx";
 import Tags from "./Tags.jsx";
 
+const now = new Date().getFullYear()
+const age = now - 2005
+
 const Me = () => {
     return (
         <>
             <Status />
             <Photo />
             <div className="name">
-                Равиль, 18 лет <span className="time">GMT +5</span>
+                Равиль, { age } лет <span className="time">GMT +5</span>
             </div>
             <Social />
             <Tags />
